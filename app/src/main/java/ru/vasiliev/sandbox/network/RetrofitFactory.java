33718d9844f47a879c2333d06bfbb5f73d9bb0fd
Kirill Vasiliev <vasiliev.k.v@gmail.com>
2019-07-07 16:59:08 +0300

    Network client factory.

---
+++ app/src/main/java/ru/vasiliev/sandbox/network/RetrofitFactory.java
@@ -0,0 +1,20 @@
+package ru.vasiliev.sandbox.network;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Date: 06.07.2019
+ *
+ * @author Kirill Vasiliev
+ */
+public class RetrofitFactory {
+
+    public static Retrofit getRetrofit(String baseUrl, OkHttpClient client) {
+        return new Retrofit.Builder().baseUrl(baseUrl).client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build();
+    }
+}
